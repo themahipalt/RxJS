273d18d487ce573b7b1002cdabf2f17c5e4374aa
Mahipal Thakur <mahipalthakur@Mahipals-MacBook-Air.local>
2024-09-29 01:47:06 +0530

    Improved code

--- exercise files/02_03/Starter Code/src/app/app.component.ts
+++ exercise files/02_03/Starter Code/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 const SNOWMAN_IMAGE = '..\\assets\\icons\\snowman image.jpg';
 const SUN_IMAGE = '..\\assets\\icons\\sun.jpg';
@@ -8,18 +10,31 @@ const SUN_IMAGE = '..\\assets\\icons\\sun.jpg';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  inputTemperature = 0;
-  imageSrc = SUN_IMAGE;
+export class AppComponent implements OnInit, OnDestroy {
+  inputTemperature: number = 0;
+  imageSrc: string = SUN_IMAGE;
+  temperatureSubject$ = new BehaviorSubject<number>(80);
+  private destroy$ = new Subject<void>(); // Used to manage unsubscriptions
 
   ngOnInit() {
+    this.temperatureSubject$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((temperature) => {
+        this.imageSrc = temperature >= 50 ? SUN_IMAGE : SNOWMAN_IMAGE;
+      });
   }
 
   setTemperature() {
+    this.temperatureSubject$.next(this.inputTemperature);
   }
 
   setInputTemperature(event: Event) {
     const input = (event.target as HTMLInputElement).value;
-    this.inputTemperature = parseInt(input);
+    this.inputTemperature = parseInt(input, 10); // Parsing input as base-10 integer
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
